33373557bb27887705a96e4a21997283037ec7d7
Huiguang Yi <yhg926@gmail.com>
2018-03-22 17:46:31 +0800

    cast to double for division before compare to JCLB

--- 4gmassive_co2dist.c
+++ 4gmassive_co2dist.c
@@ -28,25 +28,9 @@
 #define HKL 2048 //up limit of species in a hashtable(2^11)
 #define h1(k) ((k)%(HASHSIZE)) 
 #define h2(i) (i*(i+1)/2) // i > HKN^(1/2) ,i=21
-#define hs(k,i) ((h1(k) + h2(i)) % HASHSIZE)
+//#define hs(k,i) ((h1(k) + h2(i)) % HASHSIZE)
 //hash fuc for collision of same ctx in different sp.(linear proble)
-#define hg(k,i) ((h1(k) + i) % HASHSIZE) // i start for 0
-/*
-		llong tuple = 0LLU;
-		while (!feof(fp) && fread(&tuple, 8, 1, fp)) {
-			for (unsigned int offset = 0; offset < HASHSIZE; offset++) {
-				unsigned int ind = hs((tuple & objmask), offset);
-				if (CO[ind] == 0) {
-					CO[ind] = tuple;
-					break;
-				}
-			}
-		}
-		fclose(fp);
-
-*/
-
-
+//#define hg(k,i) ((h1(k) + i) % HASHSIZE) // i start for 0
 static llong objmask = OBJMASK;
 static llong tupmask =TUPMASK;
 
@@ -91,14 +75,14 @@ int main(int argc, char *argv[])
 	struct dirent *dirent;
 	int n = 0;
 	llong size=0;
-	int pw=(int)pow(HKN,0.5);
+	//int pw=(int)pow(HKN,0.5);
 	//printf("pw=%d\n",pw);
 	while ((dirent = readdir(dh)) != NULL) {
 		char *substr = strstr(dirent->d_name, ".co\0"); //end with .co not match .co
 		if (substr) {
 			snprintf(fullname, 512, "%s/%s", dirpath, dirent->d_name);
 			stat(fullname, &statbuf);
-			size += statbuf.st_size ;
+			size += statbuf.st_size/8 ;
 			 if(size < HASHSIZE * LF ){	
 				strncpy(cofile[n], dirent->d_name, 512);
 				fsize[n] = statbuf.st_size/8;
@@ -163,11 +147,13 @@ llong hcl=HASHSIZE ;// hash collision limited to HASHSIZE
 					err(errno,"#reprobe > hcl");
 			}
 			CO[ind]= spc|tuple ;
-
 		}
+		//----------------hash end----------------------//
 		for(int i=0;i<j;i++){
-			if ( (CTX[i] >SCLB)&& (CTX[i]/(fsize[i]+fsize[j]-CTX[i]) > JCLB ) && ((double)OBJ[i] / (double)CTX[i] < ULB) )
-				MAT(j, i) = MAT(i, j) = (double)OBJ[i] / (double)CTX[i];
+//!!!CTX[i]/(fsize[i]+fsize[j]-CTX[i]) == 0,make sure convert to double before compare division with a flot 
+			if ( (CTX[i] >SCLB)&& ((double)CTX[i]/(fsize[i]+fsize[j]-CTX[i]) > JCLB ) 
+			&& ((double)OBJ[i] / (double)CTX[i] < ULB) )
+				MAT(j, i) = MAT(i, j) = (double)OBJ[i] / CTX[i];
 			else
 				MAT(j, i) = MAT(i, j) = (double)URD;
 		}
@@ -181,12 +167,11 @@ llong hcl=HASHSIZE ;// hash collision limited to HASHSIZE
 		if (ptr) {
 			*ptr = '\0';
 		}
-		printf("%-9s", cofile[j]);
+		printf("%-10s", cofile[j]);
 		for (int k = 0; k < n; k++) {
 			printf(" %lf", MAT(j, k));
 		}
 		printf("\n");
 	}
-
 	return 0;
 }
